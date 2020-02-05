80c68402fc3b717741d728fea53b99dabb2e3670
SleepyCrat <frankjones04@gmail.com>
2020-02-04 21:24:52 -0600

    added firebase module stuff

--- FirebaseAndAngularAndDotnetCore/client-app/src/app/app.module.ts
+++ FirebaseAndAngularAndDotnetCore/client-app/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SecretComponent } from './secret/secret.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { SecretComponent } from './secret/secret.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
