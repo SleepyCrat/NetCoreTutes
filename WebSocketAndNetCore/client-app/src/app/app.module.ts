97d289a3c296883dd355bb68d6c3ba5d8384e112
Frank Jones <frankjones04@gmail.com>
2020-02-06 11:48:51 -0600

    just removed home component

--- WebSocketAndNetCore/client-app/src/app/app.module.ts
+++ WebSocketAndNetCore/client-app/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
